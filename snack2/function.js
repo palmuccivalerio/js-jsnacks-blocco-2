3cdd6534474b8b14aa2d3f0d33743c462de9f4fb
palmuccivalerio <palmuccivalerio@gmail.com>
2024-04-05 12:40:47 +0200

    add:fun

---
+++ snack2/function.js
@@ -0,0 +1,11 @@
+function lunghezzaWrd (word1 , word2){
+    if(word1=== word2){
+        console.log( word1, word2, "SONO UGUALI")
+
+    }
+    else(word1 > word2) 
+       console.log(word1)
+    
+
+    return lunghezzaWrd
+}
---
+++ snack2/script.js
@@ -0,0 +1,12 @@
+
+
+
+const userWrld1 = prompt("inserisci  prima parola");
+const userWrld2 = prompt ("inserisci la seconda parola");
+
+
+const lunghezza1 = userWrld1.length;
+const lunghezza2 = userWrld2.length;
+
+lunghezzaWrd(lunghezza1,lunghezza2);
+
