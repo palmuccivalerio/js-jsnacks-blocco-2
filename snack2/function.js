8a7e5fc8731ebfcac5dc66ac9202e7b058d63d25
palmuccivalerio <palmuccivalerio@gmail.com>
2024-04-05 12:46:49 +0200

    add:funct

--- snack2/function.js
+++ snack2/function.js
@@ -3,9 +3,11 @@ function lunghezzaWrd (word1 , word2){
         console.log( word1, word2, "SONO UGUALI")
 
     }
-    else(word1 > word2) 
+    else if(word1 > word2) 
        console.log(word1)
     
+    else (word1<word2)
+    console.log(word2)
 
     return lunghezzaWrd
 }
