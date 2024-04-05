16747ff14a9cd37b0b47d51a686c702bd7e44f73
palmuccivalerio <palmuccivalerio@gmail.com>
2024-04-05 11:42:50 +0200

    add:null

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
+
+
+Esercizio 1:
+Fai inserire un numero, che chiameremo N, all’utente.
+Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
+Ogni volta che ne crei uno, stampalo a schermo.
+
+
+
+
+
+
 const userNum= parseInt (prompt("iNSERISCI UN NUMERO")) 
 
 for (let i = 0; i < userNum; i++) {
     // Array per memorizzare i numeri casuali
     let randomNumbers = [];
+    let rdnNmB = (Math.floor(Math.random)*100)+1;
+}
